3592572e660a39d964d42729c1f95a572ab7b842
sapppy22 <21uec119@lnmiit.ac.in>
2025-10-06 20:12:08 +0530

    "Pricing added"

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,66 +1,102 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "./Pricing.css"
 
 const Pricing = () => {
+    const[billing,setBilling]=useState("yearly");
   return (
-    <div className='Plans'>
+    <div className='pricing-container'>
         <h2 className='Heading'>Discover the Full Potential of CoinXC</h2>
-        <p>14 days money-back guarentee .Cancel anytime,anywhere for any reason.</p>
+        <p className="pricing-para">14 days money-back guarentee .Cancel anytime for any reason.
+            Explore coinXC without any limitations with all features🚀.
+        </p>
+
+        <div className="billing-toggle">
+        <button className={billing==="monthly"?"active":""}
+         onClick={()=>setBilling("monthly")}>
+            1 Month
+         </button>
+
+         <button 
+         className={billing==="yearly"?"active":""}
+         onClick={()=>setBilling("yearly")}>
+            12 Months <span>(save up to 58% with Yearly)</span>
+         </button>
+        </div>
+
+
         <div className='plan-grid'>
-            <div className='Basic'>
+            <div className='plan-card Basic-card'>
+                <p className="current plan">CURRENT PLAN</p>
                 <h3>Basic</h3>
                 <p>Free</p>
-                <button className='basic-btn'>Select</button>
-                <li>✅Portfolio Heatmap</li>
-                <li>✅Asset Allocation Analytics</li>
-                <li>✅Profit&Loss</li>
-                <li>✅Custom Alerts</li>
-                <li>❌Add Free</li>
-                <li>❌Portfolio Performance Analysis</li>
-                <li>❌Wallet Analyser</li>
-                <li>❌Coin Insights</li>
-                <li>❌Token Risks</li>
+                <button className='plan-btn basic-btn'>Select</button>
+                <p>✅Portfolio Heatmap</p>
+
+                <p> ✅Asset Allocation Analytics</p>
+
+                <p> ✅Profit&Loss</p>
+                    
+                <p> ✅Custom Alerts</p>
+
+                <p> ❌Add Free</p>
+
+                <p> ❌Portfolio Performance Analysis</p>
+
+                <p>❌Wallet Analyser</p>
+
+                <p>❌Coin Insights</p>
+
+                <p>❌Token Risks</p>
+                
             </div>
 
-            <div className='Premium'>
+            <div className='plan-card Premium-card'>
+                <div className="best-value">BEST VALUE</div>
                 <h3>Premium</h3>
-                <div>
-                    <p>$1.66</p>
-                    <p>/months</p>
-                </div>
-                <p>You are saving 58% with the Yeartly plan</p>
-                <button className='premium-btn'>Select</button>
-                <li>✅Portfolio Heatmap</li>
-                <li>✅Asset Allocation Analytics</li>
-                <li>✅Profit&Loss</li>
-                <li>✅Custom Alerts</li>
-                <li>✅Add Free</li>
-                <li>✅Portfolio Performance Analysis</li>
-                <li>✅Wallet Analyser</li>
-                <li>❌Coin Insights</li>
-                <li>❌Token Risks</li>
+                <p className="price">
+                    ${billing==="yearly"?"1.66":"3.99"}
+                    <span>/month</span>
+                </p>
+                <p className='saving-text'>
+                    you're saving 58% with yearly plan
+                </p>
+                <button className='plan-btn premium-btn'>Select</button>
+                <a href='#' className='crypto-link'>Pay with Crypto </a>
+                <p>✅Portfolio Heatmap</p>
+                <p>✅Asset Allocation Analytics</p>
+                <p>✅Profit&Loss</p>
+                <p>✅Custom Alerts</p>
+                <p>✅Add Free</p>
+                <p>✅Portfolio Performance Analysis</p>
+                <p>✅Wallet Analyser</p>
+                <p>❌Coin Insights</p>
+                <p>❌Token Risks</p>
             </div>
 
-             <div className='Degen'>
+             <div className='plan-card Degen-card'>
                 <h3>Degen</h3>
-                <div>
-                    <p>$15.91</p>
-                    <p>/months</p>
-                </div>
-                <p>You are saving 49% with the Yeartly plan</p>
-                <button className='Degen-btn'>Select</button>
-                <li>✅Portfolio Heatmap</li>
-                <li>✅Asset Allocation Analytics</li>
-                <li>✅Profit&Loss</li>
-                <li>✅Custom Alerts</li>
-                <li>✅Add Free</li>
-                <li>✅Portfolio Performance Analysis</li>
-                <li>✅Wallet Analyser</li>
-                <li>✅Coin Insights</li>
-                <li>✅Token Risks</li>
+                <p className="price">
+                    ${billing==="yearly"?"62.91" : "89.99"}
+                    <span>/month</span>
+                </p>
+                <p className="saving-text">
+                You are saving 29% with the Yearly plan
+                </p>
+                <button className='plan-btn degen-btn'>Select</button>
+                <a href="#" className='crypto-link'>Pay with Crypto</a>
+                <p>✅Portfolio Heatmap</p>
+                <p>✅Asset Allocation Analytics</p>
+                <p>✅Profit&Loss</p>
+                <p>✅Custom Alerts</p>
+                <p>✅Add Free</p>
+                <p>✅Portfolio Performance Analysis</p>
+                <p>✅Wallet Analyser</p>
+                <p>✅Coin Insights</p>
+                <p>✅Token Risks</p>
             </div>
         </div>
     </div>
   )
 }
 
-export default Pricing
+export default Pricing;
